9193e1e36f05fb4dee48bb1029b14196e3cd9c99
Tuhin <towhid.tuhin@selise.ch>
2022-01-25 11:52:57 +0600

    Add project files.

---
+++ AppleDeepLink.cs
@@ -0,0 +1,19 @@
+﻿namespace NgRockServer
+{
+    public class AppleDeepLink
+    {
+        public AppLinks applinks { get; set; }
+
+    }
+    public class AppLinks
+    {
+        public string[] apps { get; set; }
+        public AppDetails details { get; set; }
+    }
+    public class AppDetails
+    {
+        public string appID { get; set; }
+        public string[] paths { get; set; }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using NgRockServer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+app.MapGet("/apple-app-site-association", async (http) => {
+    await http.Response.WriteAsJsonAsync(new AppleDeepLink { applinks=new AppLinks { apps=new string[] { },details=new AppDetails { appID= "J52R8YYJBF.ch.selise.sln.fcsk", paths=new string[] { "/login/reset-password-verification/*"}} } });
+});
+
+app.Run();
